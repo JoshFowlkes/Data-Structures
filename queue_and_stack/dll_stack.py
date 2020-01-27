f2e354ed79eed5aee365c6204895718b3ffdbdee
Joshua George Fowlkes <Inceptive@Joshua-George-Fowlkess-iMac.local>
2020-01-27 13:26:24 -0700

    2nd push of stuff

--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -2,17 +2,37 @@ import sys
 sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
 
+
 class Stack:
+    '''
+    a Stack is First in Last out
+    Using a line of people example, its like someone cutting to the front of 
+    the line, and then going first ahead of all the others
+    '''
     def __init__(self):
         self.size = 0
         # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
 
     def push(self, value):
-        pass
+        '''
+        Much like Quene except this adds to the head rather than add to the back
+        '''
+        self.size += 1
+        self.storage.add_to_head(value)
 
     def pop(self):
-        pass
+        '''
+        Remove item from the head of the stack
+        '''
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return None
 
     def len(self):
-        pass
+        '''
+        Return the number of items in the stack
+        '''
+        return self.size
